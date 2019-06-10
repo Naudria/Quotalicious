0a36e682a7cba61d0a1f142a0bd3fc23b354aba4
Naudria <rima.jean@gmail.com>
2019-06-10 09:50:55 -0500

    Add list and quote ids to join table and tested rails server

--- db/migrate/20190610143736_create_favorite_quotes.rb
+++ db/migrate/20190610143736_create_favorite_quotes.rb
@@ -1,7 +1,8 @@
 class CreateFavoriteQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :favorite_quotes do |t|
-
+    	t.integer :quote_id
+      t.integer :list_id
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+List.create(title:"Motivational Quotes")
+List.create(title:"Inspire Me!")
+List.create(title:"For When I'm Sad")
