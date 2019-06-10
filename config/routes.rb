31057e68a1acfba83946863a134c6f6302338630
Naudria <rima.jean@gmail.com>
2019-06-10 09:33:49 -0500

    Added routes for lists and quotes. Created folder api for controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  resources :quotes
-  resources :lists
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+
+    get 'list', to: 'lists#show', as: 'list_show'
+    # post 'signup', to: 'lists#create', as: 'user_signup'
+    # post 'login', to: 'lists#login', as: 'user_login'
+
+    resources :lists do
+      get :favorites, on: :member
+      post :add_to_favorites, on: :member
+      post :remove_from_favorites, on: :member
+    end
+
+    resources :quotes do
+      get :favorited_by, on: :member
+    end
+
+  end
+
 end
