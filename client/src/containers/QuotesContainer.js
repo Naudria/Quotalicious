8b0b571ccac7d40551305fbc707a06676de0340d
Naudria <rima.jean@gmail.com>
2019-06-11 11:07:14 -0500

    Import QuoteDetail into quotes container and App.js

--- client/src/containers/QuotesContainer.js
+++ client/src/containers/QuotesContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AllQuotes from '../components/AllQuotes';
+import QuoteDetail from '../components/QuoteDetail'
 import Header from '../components/Header'
 import { Grid, Divider, Container } from 'semantic-ui-react'
 import {connect} from 'react-redux'
@@ -17,6 +18,12 @@ const QuotesContainer = () => {
           <Grid.Column width={10}>
            
             <AllQuotes />
+           
+          </Grid.Column>
+
+           <Grid.Column width={4}>
+           
+            <QuoteDetail />
            
           </Grid.Column>
           </Grid>
