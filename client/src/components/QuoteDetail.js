746769f39b1034279ad505e44b2dc20e68afecd8
Naudria <rima.jean@gmail.com>
2019-06-11 11:03:55 -0500

    Create component QuoteDetail that imports the selectQuote action.

---
+++ client/src/components/QuoteDetail.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import  { selectQuote }  from '../actions/quoteActions'
+
+class QuoteDetail extends Component {
+
+  render() {
+
+  	return (
+  		<div>Hi</div>
+  	)
+  }
+}
+
+export default QuoteDetail
