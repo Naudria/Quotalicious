57160de4c00486c38f083cb30c42a49e5a90d0fd
Naudria <rima.jean@gmail.com>
2019-06-12 08:30:12 -0500

    Map over the current list's favorite quotes in the render function of the FavoriteQuotes component and display each quote in a semantic ui card with a remove button. Wire up mapStateToProps function to find the current list.

--- client/src/components/FavoriteQuotes.js
+++ client/src/components/FavoriteQuotes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { removeQuoteFromUserFavorites } from '../actions/userActions'
 import { bindActionCreators } from 'redux'
+import { Container, Card } from 'semantic-ui-react'
 import QuoteDetail from '../components/QuoteDetail'
 import {connect} from 'react-redux'
 
@@ -9,8 +10,34 @@ class FavoriteQuotes extends Component {
   render() {
 console.log(this.props)
 
-  	return (
-  		<div>hi</div>
-  		)
+  	if (this.props.currentUser) {
+        
+        return (
+        	<Container>
+        		<Card.Group centered>
+        			{this.props.currentList.favorites.map((quote, index) => 
+        				<Card key={quote.id}>
+        				<Card.Content>
+        				{quote.author}
+        				</Card.Content>
+        				<Card.Content extra>
+        				 	<Button basic color='red'>
+            				Remove quote
+          				</Button>
+        				</Card.Content>
+        				</Card>
+        				)}
+        		</Card.Group>
+        	</Container>
+    )
+    }    
  	}
 }
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser,
+  }
+}
+
+export default connect(mapStateToProps)(FavoriteQuotes)
